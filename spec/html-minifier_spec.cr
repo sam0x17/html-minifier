1edd19f374e3ca15d2a5bdafb89f58fc4d7051ae
Sam Johnson <sam@durosoft.com>
2019-10-01 10:21:55 -0400

    initial commit

---
+++ spec/html-minifier_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Html::Minifier do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/html-minifier.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Html::Minifier`
+module Html::Minifier
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
