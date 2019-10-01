154032850ea5a98a87b79e645d57b2922b29e947
Sam Johnson <sam@durosoft.com>
2019-10-01 12:33:25 -0400

    import html-minifier.min.js v4.0.0

--- src/html-minifier.cr
+++ src/html-minifier.cr
@@ -1,4 +1,12 @@
 require "duktape"
+require "baked_file_system"
+
+class HtmlMinifierNpm
+  extend BakedFileSystem
+  bake_folder "../html-minifier"
+end
+HTML_MINIFIER_JS = HtmlMinifierNpm.get("html-minifier.min.js").gets_to_end
+
 
 module HtmlMinifier
 
